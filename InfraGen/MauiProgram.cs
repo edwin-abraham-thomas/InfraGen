bb11f8eb33d4e4a13ed933cb7b937114124ce560
Edwin Abraham Thomas <edwinat.dev@outlook.com>
2023-02-09 23:46:52 +0530

    Added basinc user setting file

--- InfraGen/MauiProgram.cs
+++ InfraGen/MauiProgram.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.Extensions.Logging;
 using InfraGen.Data;
+using InfraGen.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace InfraGen;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
+
+	private static string _userFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+    public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
@@ -17,8 +23,11 @@ public static class MauiProgram
 
 		builder.Services.AddMauiBlazorWebView();
 
+        EnsureDirectoryAndSettingFileExist();
+        InitUserFromLocalSettingFile();
+
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
 
@@ -26,4 +35,34 @@ public static class MauiProgram
 
 		return builder.Build();
 	}
+
+	private static User InitUserFromLocalSettingFile()
+	{
+        using (StreamReader file = File.OpenText($"{_userFilePath}\\infragen\\user.json"))
+        using (JsonTextReader reader = new JsonTextReader(file))
+		{
+            var userString = JToken.ReadFrom(reader).ToString();
+			var user = JsonConvert.DeserializeObject<User>(userString);
+        }
+
+        return null;
+    }
+
+	private static void EnsureDirectoryAndSettingFileExist()
+	{
+
+		var isFolderPresent = Directory.Exists($"{_userFilePath}\\infragen");
+		if(!isFolderPresent)
+		{
+			Directory.CreateDirectory($"{_userFilePath}\\infragen");
+		}
+
+		var isUserFileExist = File.Exists($"{_userFilePath}\\infragen\\user.json");
+		if(!isUserFileExist)
+		{
+            var user = new User();
+            var userJson = JsonConvert.SerializeObject(user);
+            File.WriteAllText($"{_userFilePath}\\infragen\\user.json", userJson);
+        }
+    }
 }
